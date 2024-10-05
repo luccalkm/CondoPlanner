2ba03669517ab09a029285d9112113bae7f574db
Ana Luiza Favacho <analufavacho@gmail.com>
2024-10-04 22:36:39 -0300

    ajustando pastas

--- condo-app/src/App.tsx
+++ condo-app/src/App.tsx
@@ -1,12 +1,16 @@
 import { Route, Routes } from "react-router-dom";
 import LoginPage from "./pages/login/Login";
-import LoginLayout from "./components/layout/LoginLayout";
+import LoginLayout from "./layouts/LoginLayout";
 import RegisterPage from "./pages/register/Register";
+import MainLayout from "./layouts/MainLayout";
 
 function App() {
     return (
         <>
             <Routes>
+                <Route element={<MainLayout />}>
+                    <Route path="/home" element={<HomePage />} />
+                </Route>
                 <Route element={<LoginLayout />}>
                     <Route path="/login" element={<LoginPage />} />
                     <Route path="/register" element={<RegisterPage />} />
