93b070f644420b08bfddabc0cb39dd29f3fdae9a
Ana Luiza Favacho <analufavacho@gmail.com>
2024-10-04 22:38:20 -0300

    add home

--- condo-app/src/App.tsx
+++ condo-app/src/App.tsx
@@ -3,6 +3,7 @@ import LoginPage from "./pages/login/Login";
 import LoginLayout from "./layouts/LoginLayout";
 import RegisterPage from "./pages/register/Register";
 import MainLayout from "./layouts/MainLayout";
+import HomePage from "./pages/home/Home";
 
 function App() {
     return (
---
+++ condo-app/src/pages/home/Home.tsx
@@ -0,0 +1,9 @@
+const HomePage = () => {
+    return (
+        <>
+        <p>HOme</p>
+        </>
+    )
+};
+
+export default HomePage;
